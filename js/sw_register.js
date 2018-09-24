158d5aadaafd565925b17dd4efbe28a6bb0b2bc8
Michael <arrietem@gmail.com>
2018-09-24 16:38:31 -0400

    Added Service Worker to project

---
+++ js/sw_register.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('./sw.js').
+		then(function() {
+			console.log('Registration worked!');
+		})
+		.catch(function() {
+			console.log('Registration failed!');
+		});
+	});
+}
